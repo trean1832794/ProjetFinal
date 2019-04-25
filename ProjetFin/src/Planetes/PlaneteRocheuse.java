eb39102ee195142ea89f669733036ee998fd3680
berth1832777 <berth1832777@etu.cegepjonquiere.ca>
2019-04-25 14:48:45 -0400

    Planete a 100% de materiau changée pour 20% chacun

--- ProjetFin/src/Planetes/PlaneteRocheuse.java
+++ ProjetFin/src/Planetes/PlaneteRocheuse.java
@@ -4,7 +4,7 @@ public class PlaneteRocheuse extends Planete {
 
     public PlaneteRocheuse () {
         //0 = plutonium 1 = thulium 2 = gadolium 3 = terbium 4 = neptunium
-        super(new float[]{0.0f,0.0f,0.0f,1.0f,0.0f});
+        super(new float[]{0.2f,0.4f,0.6f,0.8f,1.0f});
 
     }
 
